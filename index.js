2dcd99dbf4c65355b3747c26836f75e240d8c42d
ebukaakara <the1stnigerian@gmail.com>
2020-09-30 17:04:09 +0100

    first commit

--- index.js
+++ index.js
@@ -1,51 +1,159 @@
-const carouselSlide = document.querySelector(
-   '.carousel-slide'
+class Calculator {
+   constructor(
+      previousOperandTextElement,
+      currentOperandTextElement
+   ) {
+      this.previousOperandTextElement = previousOperandTextElement;
+      this.currentOperandTextElement = currentOperandTextElement;
+      this.clear();
+   }
+
+   clear() {
+      this.currentOperand = '';
+      this.previousOperand = '';
+      this.operation = undefined;
+   }
+
+   delete() {
+      this.currentOperand = this.currentOperand
+         .toString()
+         .slice(0, -1);
+   }
+
+   appendNumber(number) {
+      if (
+         number === '.' &&
+         this.currentOperand.includes('.')
+      )
+         return;
+      this.currentOperand =
+         this.currentOperand.toString() + number.toString();
+   }
+
+   chooseOperation(operation) {
+      if (this.currentOperand === '') return;
+      if (this.previousOperand !== '') {
+         this.compute();
+      }
+      this.operation = operation;
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = '';
+   }
+
+   compute() {
+      let computation;
+      const prev = parseFloat(this.previousOperand);
+      const current = parseFloat(this.currentOperand);
+      if (isNaN(prev) || isNaN(current)) return;
+      switch (this.operation) {
+         case '+':
+            computation = prev + current;
+            break;
+         case '-':
+            computation = prev - current;
+            break;
+         case '*':
+            computation = prev * current;
+            break;
+         case '÷':
+            computation = prev / current;
+            break;
+         default:
+            return;
+      }
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.previousOperand = '';
+   }
+
+   getDisplayNumber(number) {
+      const stringNumber = number.toString();
+      const integerDigits = parseFloat(
+         stringNumber.split('.')[0]
+      );
+      const decimalDigits = stringNumber.split('.')[1];
+      let integerDisplay;
+      if (isNaN(integerDigits)) {
+         integerDisplay = '';
+      } else {
+         integerDisplay = integerDigits.toLocaleString(
+            'en',
+            { maximumFractionDigits: 0 }
+         );
+      }
+      if (decimalDigits != null) {
+         return `${integerDisplay}.${decimalDigits}`;
+      } else {
+         return integerDisplay;
+      }
+   }
+
+   updateDisplay() {
+      this.currentOperandTextElement.innerText = this.getDisplayNumber(
+         this.currentOperand
+      );
+      if (this.operation != null) {
+         this.previousOperandTextElement.innerText = `${this.getDisplayNumber(
+            this.previousOperand
+         )} ${this.operation}`;
+      } else {
+         this.previousOperandTextElement.innerText = '';
+      }
+   }
+}
+
+const numberButtons = document.querySelectorAll(
+   '[data-number]'
 );
-const carouselImages = document.querySelectorAll(
-   '.carousel-slide img'
+const operationButtons = document.querySelectorAll(
+   '[data-operation]'
+);
+const equalsButton = document.querySelector(
+   '[data-equals]'
+);
+const deleteButton = document.querySelector(
+   '[data-delete]'
+);
+const allClearButton = document.querySelector(
+   '[data-all-clear]'
+);
+const previousOperandTextElement = document.querySelector(
+   '[data-previous-operand]'
+);
+const currentOperandTextElement = document.querySelector(
+   '[data-current-operand]'
 );
 
-// Buttons
-const prevbtn = document.querySelector('#prev');
-const nextbtn = document.querySelector('#next');
+const calculator = new Calculator(
+   previousOperandTextElement,
+   currentOperandTextElement
+);
 
-// Counter
-let counter = 1;
-const size = carouselImages[0].clientWidth;
-
-carouselSlide.style.transform =
-   'translateX(' + -size * counter + 'px)';
-
-// Button events
-nextbtn.addEventListener('click', () => {
-   if (counter >= carouselImages.length - 1) return;
-   carouselSlide.style.transition =
-      'transform 0.8s ease-in-out';
-   counter++;
-   carouselSlide.style.transform =
-      'translateX(' + -size * counter + 'px)';
+numberButtons.forEach((button) => {
+   button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText);
+      calculator.updateDisplay();
+   });
 });
 
-prevbtn.addEventListener('click', () => {
-   if (counter <= 0) return;
-   carouselSlide.style.transition =
-      'transform 0.8s ease-in-out';
-   counter--;
-   carouselSlide.style.transform =
-      'translateX(' + -size * counter + 'px)';
+operationButtons.forEach((button) => {
+   button.addEventListener('click', () => {
+      calculator.chooseOperation(button.innerText);
+      calculator.updateDisplay();
+   });
 });
 
-carouselSlide.addEventListener('transitionend', () => {
-   if (carouselImages[counter].id === 'lastClone') {
-      carouselSlide.style.transform = 'none';
-      counter = carouselImages.length - 2;
-      carouselSlide.style.transform =
-         'translateX(' + -size * counter + 'px)';
-   }
-   if (carouselImages[counter].id === 'firstClone') {
-      carouselSlide.style.transform = 'none';
-      counter = carouselImages.length - counter;
-      carouselSlide.style.transform =
-         'translateX(' + -size * counter + 'px)';
-   }
+equalsButton.addEventListener('click', (button) => {
+   calculator.compute();
+   calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', (button) => {
+   calculator.clear();
+   calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', (button) => {
+   calculator.delete();
+   calculator.updateDisplay();
 });
