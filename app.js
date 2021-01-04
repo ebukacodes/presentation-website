5f9c50e185fb127880d627d8310fe8e6df13e55a
ebukaakara <the1stnigerian@gmail.com>
2021-01-04 15:53:17 +0100

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
 
-signUpButton.addEventListener('click', () => {
-  container.classList.add('right-panel-active');
-});
+let load = 0;
 
-signInButton.addEventListener('click', () => {
-  container.classList.remove('right-panel-active');
-});
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)})`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return (
+    ((num - in_min) * (out_max - out_min)) /
+      (in_max - in_min) +
+    out_min
+  );
+};
