aaaa149b4117414ac2c14dcc7947bad2dd18a82b
ebukaakara <the1stnigerian@gmail.com>
2021-01-05 10:08:29 +0100

    first commit

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
+const menuToggle = document.querySelector('.toggle');
+const showcase = document.querySelector('.showcase');
 
-let load = 0;
-
-let int = setInterval(blurring, 30);
-
-function blurring() {
-  load++;
-
-  if (load > 99) {
-    clearInterval(int);
-  }
-
-  loadText.innerHTML = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)})`
-}
-
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return (
-    ((num - in_min) * (out_max - out_min)) /
-      (in_max - in_min) +
-    out_min
-  );
-};
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('active');
+  showcase.classList.toggle('active');
+});
